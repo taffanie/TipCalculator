8fcf05016a2eb7bcf3b8345084f3a275dc423a8e
Taffanie Ben <taffanie.ben@gmail.com>
2020-01-04 14:35:37 +0100

    First upload
    Errors:
    1. when entering bill amounts with decimal places, "total bill" does not respond
    2. when page shrinks, last 3 elements do not stay fixed (position issue)
    3. position of all elements not in line or centre
    To improve functionality:
    1. change result to 2dp
    2. use flexbox for positioning?

---
+++ script.js
@@ -0,0 +1,29 @@
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value + "%"; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value + "%";
+  }
+
+let bill = document.getElementById('bill');
+
+bill.addEventListener('input', function() {
+});
+
+document.getElementById('calculate').addEventListener('click', function() {
+    let tiptotal = (bill.value * slider.value /100);
+    if (bill.value) {
+        tipamount.innerHTML = "Tip Amount: " + "£" + tiptotal; 
+    }
+    let billtotal = (parseInt(bill.value) + parseInt(tiptotal));
+    if (bill.value) {
+        totalbill.innerHTML = "Total Bill: " + "£" + billtotal;
+    }
+ });
+
+let tipamount = document.getElementById('tipamount');
+let totalbill = document.getElementById('totalbill');
+
+ 
